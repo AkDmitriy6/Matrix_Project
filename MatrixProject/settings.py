67b9906abfc88e82835ffa9b325b15b8a56f3d5f
Dmitriy <dimas.akimenkov@yandex.ru>
2022-11-12 15:31:20 +0300

    second edit

--- MatrixProject/settings.py
+++ MatrixProject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rankapp',
     'api',
     'rest_framework',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,14 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Rank API',
+    'DESCRIPTION': 'API that get rank of the matrix',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
--- api/models.py
+++ api/models.py
@@ -1,6 +1,5 @@
-
-class Rank:
-    def __init__(self, cols, rows, args, result):
+class Rank:  # создание класса
+    def __init__(self, cols, rows, args, result):  # Инициализация переменных
         self.cols = cols
         self.rows = rows
         self.args = args
